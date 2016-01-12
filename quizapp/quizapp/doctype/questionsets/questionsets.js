43dbcf888ebfb56dbd6dc3af18c5fd83293df9b5
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-01-12 18:32:21 +0530

    Done Validations

---
+++ quizapp/quizapp/doctype/questionsets/questionsets.js
@@ -0,0 +1,14 @@
+frappe.ui.form.on("Questionsets", "passing_marks", function(frm) {
+  console.log(cur_frm.doc.passing_marks);
+  console.log(cur_frm.doc.marks_per_question);
+
+  if (cur_frm.doc.passing_marks < cur_frm.doc.marks_per_question)
+  {
+    cur_frm.doc.passing_marks="";
+    //refrsh_field("passing_marks")
+    msgprint(__("Passing marks can't be less than marks per correct answer"));
+
+
+  }
+
+});
