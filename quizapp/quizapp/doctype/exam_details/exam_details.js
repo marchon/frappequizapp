416b14ca60d3eb85f90dcd35acb523121e726ce0
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-01-13 12:41:11 +0530

    Added a final touch to the clientside application with date validations

--- quizapp/quizapp/doctype/exam_details/exam_details.js
+++ quizapp/quizapp/doctype/exam_details/exam_details.js
@@ -1,11 +1,22 @@
 frappe.ui.form.on("Exam Details", "end_time", function(frm) {
   console.log(cur_frm.doc.passing_marks);
-  console.log(cur_frm.doc.marks_per_question);
+  console.log(frappe.datetime.nowdate());
 
   if (cur_frm.doc.end_time < cur_frm.doc.start_time)
   {
   frm.set_value("end_time","")
     msgprint(__("End time can't be less than Start Time"));
+  }
+
+});
+frappe.ui.form.on("Exam Details", "date", function(frm) {
+  console.log(cur_frm.doc.passing_marks);
+  console.log(frappe.datetime.nowdate());
+
+  if (cur_frm.doc.date < frappe.datetime.nowdate())
+  {
+  frm.set_value("date","")
+    msgprint(__("Date can't be less than Today's date"));
   }
 
 });
